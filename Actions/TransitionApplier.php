4a7876b5958c7f686cdc08ced8e5311e435de4a6
fduch <alex.medwedew@gmail.com>
2017-02-07 16:49:27 +0300

    rethrow exceptions in case of domain exceptions/errors

--- Actions/TransitionApplier.php
+++ Actions/TransitionApplier.php
@@ -70,12 +70,17 @@ class TransitionApplier
      */
     public function applyTransitions(WorkflowContext $workflowContext, array $transitions = [], $cascade = false)
     {
+        if (!$transitions) {
+            return;
+        }
+
         $workflow      = $workflowContext->getWorkflow();
         $subject       = $workflowContext->getSubject();
         $loggerContext = $workflowContext->getLoggerContext();
 
         $this->logger->debug('Resolved workflow for subject', $loggerContext);
 
+        $applied = false;
         foreach ($transitions as $transition) {
             try {
                 // We do not call Workflow:can method here due to performance reasons in order to prevent
@@ -83,12 +88,12 @@ class TransitionApplier
                 // Therefore we catch LogicException and interpreting it as case when transition can not be applied
                 $workflow->apply($subject, $transition);
                 $this->logger->info(sprintf('Workflow successfully applied transition "%s"', $transition), $loggerContext);
-
+                $applied = true;
                 if (!$cascade) {
                     break;
                 }
             } catch (WorkflowLogicException $e) {
-                // transition cannot be applied because of it is not allowed
+                // transition cannot be applied because it is not allowed
                 $this->logger->info(
                     sprintf('Workflow transition "%s" cannot be applied due to it is not allowed', $transition),
                     $loggerContext
@@ -98,12 +103,18 @@ class TransitionApplier
                     sprintf('Workflow cannot apply transition "%s" due to exception. Details: %s', $transition, $e->getMessage()),
                     $loggerContext
                 );
+                throw $e;
             } catch (Throwable $e) {
                 $this->logger->critical(
-                    sprintf('Workflow cannot apply transition "%s" due to exception. Details: %s', $transition, $e->getMessage()),
+                    sprintf('Workflow cannot apply transition "%s" due to error. Details: %s', $transition, $e->getMessage()),
                     $loggerContext
                 );
+                throw $e;
             }
+        }
+
+        if (!$applied) {
+            $this->logger->warning('All transitions to apply are not allowed', $loggerContext);
         }
     }
 }
